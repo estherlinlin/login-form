e8af3ec238d3de12c3ab7ee2caa2fcb19088674d
EstherLin <pinchunchun@gmail.com>
2020-07-05 18:38:59 +0800

    modify typelist data

--- src/components/Auth/AccountTypeList/AccountTypeList.js
+++ src/components/Auth/AccountTypeList/AccountTypeList.js
@@ -3,8 +3,17 @@ import "./AccountTypeList.scss";
 
 import accountTypes from "./accounTypeData";
 import AccountType from "../AccountType/AccountType";
+import doctorImg from "../../../assets/images/img_doctor_90@3x.svg";
+import patientImg from "../../../assets/images/img_patient_90@3x.svg";
 
 const AccountTypeList = ({ selectedType, typeSelectHandler }) => {
+  const accountTypes = [
+    { name: "doctor", img: doctorImg },
+    { name: "patient", img: patientImg }
+    // { name: "patient2", img: patientImg },
+    // { name: "patient3", img: patientImg },
+  ];
+
   return (
     <div className="account-container">
       {accountTypes.map((item) => (
