98d9f59e3bc3f7d11d162b6ca4fe455fa25be4c2
EstherLin <pinchunchun@gmail.com>
2020-07-05 19:55:17 +0800

    Updates

---
+++ precache-manifest.c67967f28ae8d1feaf3614b9cb9f2ce4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416b92a1c6682ff27a6780cf7956ec51",
+    "url": "/login-form/index.html"
+  },
+  {
+    "revision": "79e534b2e6d12ed6fd7e",
+    "url": "/login-form/static/css/main.e1680f36.chunk.css"
+  },
+  {
+    "revision": "b96e48e995d864f220d5",
+    "url": "/login-form/static/js/2.c78e7dc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login-form/static/js/2.c78e7dc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e534b2e6d12ed6fd7e",
+    "url": "/login-form/static/js/main.c33e8bb9.chunk.js"
+  },
+  {
+    "revision": "6274f5f32af268186927",
+    "url": "/login-form/static/js/runtime-main.1278f55b.js"
+  },
+  {
+    "revision": "d57aad92c53caee384906220fe488dfd",
+    "url": "/login-form/static/media/img_doctor_90@3x.d57aad92.svg"
+  },
+  {
+    "revision": "3cbdeb85bfe6d587ae9e3d2a801edb65",
+    "url": "/login-form/static/media/img_patient_90@3x.3cbdeb85.svg"
+  },
+  {
+    "revision": "a01c3092303031ec6211f6f4c45be3a7",
+    "url": "/login-form/static/media/img_town_370x170@3x.a01c3092.svg"
+  }
+]);
